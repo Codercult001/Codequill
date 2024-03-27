4b96dc435e3e924d955f4fd3979303855c7c1365
Codercult001 <ramanaggrawal2@gmail.com>
2024-03-27 19:46:24 +0530

    git commit -m "the files wasn't properly"

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import decode from 'jwt-decode'
@@ -13,13 +13,13 @@ const Navbar = () => {
     const dispatch = useDispatch()
     var User = useSelector((state) => (state.currentUserReducer))
     const navigate = useNavigate();
-    
-    const handleLogout = () => {
+
+    const handleLogout = useCallback(() => {
         dispatch({ type: 'LOGOUT'});
         navigate('/')
         dispatch(setCurrentUser(null))
-    }
-    
+    }, [dispatch, navigate])
+
     useEffect(() => {
         const token = User?.token 
         if(token){
@@ -29,7 +29,7 @@ const Navbar = () => {
             }
         }
         dispatch(setCurrentUser( JSON.parse(localStorage.getItem('Profile'))))
-    },[User?.token, dispatch ])
+    }, [User?.token, dispatch, handleLogout])
 
     return (
         <nav className='main-nav'>
